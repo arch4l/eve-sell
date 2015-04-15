d49ba167347b2cd78c0c2e9c90509c5695fac328
arch4l <archal@gmx.ch>
2015-04-15 10:45:06 +0200

    intial

---
+++ eve-sell.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: EVE Sell
+* Plugin URI: http://foo.bar/
+* Description: A place where you can sell some items.
+* Version: 0.1
+* Author: Archal
+* Author URI: 
+* License: A "Slug" license name e.g. GPL12
+*/
